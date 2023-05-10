8b52de13cc9c4dc450618d593c86d59b0ab7c653
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-05-10 12:45:52 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import Dashboard from "./componentes/Dashboard";
+import Home from "./componentes/Home";
+import About from "./componentes/About";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Router>
+      <div>
+        <ul>
+        <li><Link to="/">Home</Link></li>   
+        <li><Link to="/componentes/About">About</Link></li>
+        <li><Link to="/componentes/Dashboard">Dashboard</Link></li>
+        </ul>
+       </div>
+      <Routes>
+        <Route path="/"element={<Home/>}/>
+        <Route path="/componentes/About"element={<About/>}/>
+        <Route path="/componentes/Dashboard"element={<Dashboard/>}/>
+      </Routes>
+
+    
+    </Router>
+  )
 }
+
+
 
 export default App;
